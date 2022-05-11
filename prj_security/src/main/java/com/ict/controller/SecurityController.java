7cafdd0dd6c71a319a46a0a6eecb01d32fda660a
jjjjjjji-hun <lingcke@naver.com>
2022-05-11 11:54:42 +0900

    스프링31일차

--- prj_security/src/main/java/com/ict/controller/SecurityController.java
+++ prj_security/src/main/java/com/ict/controller/SecurityController.java
@@ -1,9 +1,14 @@
 package com.ict.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ict.domain.MemberVO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -12,6 +17,12 @@ import lombok.extern.log4j.Log4j;
 @Controller
 public class SecurityController {
 
+	//@Autowired
+	//private SecurityService service;
+	
+	@Autowired
+	private PasswordEncoder pwen;
+	
 	@PreAuthorize("permitAll")
 	@GetMapping("/all")
 	public void doAll() {
@@ -28,5 +39,21 @@ public class SecurityController {
 	@GetMapping("/admin")
 	public void doAdmin() {
 		log.info("운영자만 접속 가능한 admin 로직");
+	}
+	
+	@PreAuthorize("permitAll")
+	@GetMapping("/join")
+	public void joinForm() {
+		log.info("회원가입창 접속");
+	}
+	
+	@PreAuthorize("permitAll")
+	@PostMapping("/join")
+	public void join(MemberVO vo, String[] role) {
+		log.info("가입시 받는 데이터들 : " + vo);
+		log.info("사용자가 선택한 권한목록 : " + role);
+		for(String r : role) {
+			log.info(r);
+		}
 	}
 }
--- prj_security/src/main/java/com/ict/domain/MemberVO.java
+++ prj_security/src/main/java/com/ict/domain/MemberVO.java
@@ -14,5 +14,8 @@ public class MemberVO {
 	private boolean enabled;
 	private Date regDate;
 	private Date updateDate;
+	// join을 염두에 둔 VO 형태
+	// 하나의 계정에 두 개 이상의 권한이 들어가는 상황도 있기 때문에
+	// List로 처리함.
 	private List<AuthVO> authList;
 }
