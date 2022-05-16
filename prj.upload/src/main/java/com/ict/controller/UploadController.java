d4d0020369672dffd341bd1e6de5acf6d7df261c
jjjjjjji-hun <lingcke@naver.com>
2022-05-16 16:54:43 +0900

    스프링 34일차

--- prj.upload/src/main/java/com/ict/controller/UploadController.java
+++ prj.upload/src/main/java/com/ict/controller/UploadController.java
@@ -1,6 +1,12 @@
 package com.ict.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,11 +15,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @Controller
 @Log4j // pom.xml에서 junit을 4.12로, log4j는 1.2.17버전, exclusions 태그 삭제, scope주석
 public class UploadController {
 
+	private boolean checkImageType(File file) {
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+					return contentType.startsWith("image");
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		String str = sdf.format(date);
+		return str.replace("-", File.separator);
+	}
+	
 	@GetMapping("/uploadForm")
 	public void uploadForm() {
 		log.info("upload form");
@@ -24,15 +48,40 @@ public class UploadController {
 		
 		String uploadFolder = "C:\\upload_data\\temp";
 		
+		// 폴더 생성
+		File uploadPath = new File(uploadFolder, getFolder());
+		log.info("uploadPath : "  + uploadPath);
+		
+		if(uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
+		
 		for(MultipartFile multipartFile : uploadFile) {
 			log.info("---------------------");
 			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
 			log.info("Upload File Size : " + multipartFile.getSize());
 		
-			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+			String uploadFileName = multipartFile.getOriginalFilename();
+			
+			//UUID 발급 부분
+			UUID uuid = UUID.randomUUID();
+			
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			
+			//File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+			//File saveFile = new File(uploadPath, uploadFileName);
 			
 			try {
+				File saveFile = new File(uploadPath, uploadFileName);
 				multipartFile.transferTo(saveFile);
+				// 이 아래부터 썸네일 생성로직
+				if(checkImageType(saveFile)) {
+					FileOutputStream thumbnail = 
+							new FileOutputStream(
+									new File(uploadPath, "s_" + uploadFileName));
+					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+					thumbnail.close();
+				}
 			}catch(Exception e) {
 				log.error(e.getMessage());
 			}
@@ -48,7 +97,16 @@ public class UploadController {
 	public void uploadAjaxPost(MultipartFile[] uploadFile) {
 		log.info("ajax post update!");
 		
+		//어떤 폴더에 저장할 것인지 위치지정
 		String uploadFolder = "C:\\upload_data\\temp";
+		
+		// 폴더 생성
+		File uploadPath = new File(uploadFolder, getFolder());
+		log.info("uploadPath : "  + uploadPath);
+		
+		if(uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
 		
 		for(MultipartFile multipartFile : uploadFile) {
 			log.info("---------------------");
@@ -61,13 +119,29 @@ public class UploadController {
 			
 			log.info("last file name : " + uploadFileName);
 			
-			File saveFile = new File(uploadFolder, uploadFileName);
+			//UUID 발급 부분
+			UUID uuid = UUID.randomUUID();
+			
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			
+			//File saveFile = new File(uploadFolder, uploadFileName);
+			//File saveFile = new File(uploadPath, uploadFileName);
 			
 			try {
+				File saveFile = new File(uploadPath, uploadFileName);
 				multipartFile.transferTo(saveFile);
+				// 이 아래부터 썸네일 생성로직
+				if(checkImageType(saveFile)) {
+					FileOutputStream thumbnail = 
+							new FileOutputStream(
+									new File(uploadPath, "s_" + uploadFileName));
+					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+					thumbnail.close();
+				}
 			}catch(Exception e) {
 				log.error(e.getMessage());
 			}
 		}// end for
 	}
+	
 }
---
+++ prj.upload/src/main/java/com/ict/domain/AttachFileDTO.java
@@ -0,0 +1,9 @@
+package com.ict.domain;
+
+public class AttachFileDTO {
+
+	private String fileName;
+	private String uploadPath;
+	private String uuid;
+	private boolean image;
+}
